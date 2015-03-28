4631af4028d023ca5912fdad12a7ec2711c58499
AJ <avjmail@gmail.com>
2015-03-28 16:30:28 +0200

    init

---
+++ riddleboxSPA/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace riddleboxSPA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The product built by joint team during Garage 48 in Riga end of March 2015.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
